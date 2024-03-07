f57597ebb62329cb366b9d16b9c468b02b0ceab0
Monika Januszek <monika.januszek.mail@gmall.com>
2024-03-07 14:58:04 +0100

    display basket in popup window

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -1,19 +1,31 @@
 import { useBasket } from "./BasketContext";
+import React, { useState } from "react";
 
 function Basket() {
+  const [showPopup, setShowPopup] = useState(false);
   const { basketItems } = useBasket();
   function handleDisplayBasket() {
     console.log("basket");
-    // {
-    //   basketItems.map((item, index) => (
-    //     <div key={index}>
-    //       <h2>{item.name}</h2>
-    //       <p>{item.price} PLN</p>
-    //     </div>
-    //   ));
-    // }
+    setShowPopup(!showPopup);
+    console.log(showPopup, basketItems);
   }
-  return <div className="basket" onClick={handleDisplayBasket}></div>;
+  return (
+    <>
+      <div className="basket" onClick={handleDisplayBasket}></div>;
+      {showPopup && (
+        <div className="popup">
+          <div className="basket-items">
+            {basketItems.map((item, index) => (
+              <div key={index}>
+                <h2>{item.name}</h2>
+                <p>{item.price} PLN</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Basket;
