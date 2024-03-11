68d8a14ad00fd495850b6939ec6d0b38fe709bcb
Monika Januszek <monika.januszek.mail@gmall.com>
2024-03-11 12:45:12 +0100

    form component html structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
+import "../style/Contact.scss";
 
 function Contact() {
   return (
     <>
       <Navigation />
-      <p>contact</p>
+      <div className="contact">
+        <h1>Send us a message</h1>
+        <form className="contact-form">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" />
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" />
+          <label htmlFor="message">Your message</label>
+          <textarea id="message" cols="5" rows="5"></textarea>
+
+          <button type="submit" className="submit-btn">
+            Send
+          </button>
+        </form>
+      </div>
       <Footer />
     </>
   );
