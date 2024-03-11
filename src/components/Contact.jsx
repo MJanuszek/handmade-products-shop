bd43a47b75bcd5dab7f102c1cbc12cd6e2f73b16
Monika Januszek <monika.januszek.mail@gmall.com>
2024-03-11 15:15:36 +0100

    handle inputs values by useState

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import "../style/Contact.scss";
 
 function Contact() {
+  const [inputValues, setInputValues] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  function handleSubmitForm(e) {
+    e.preventDefault();
+    console.log("ok");
+    setInputValues({
+      name: "",
+      email: "",
+      message: "",
+    });
+  }
+  //
+  function handleInputChange(e) {
+    let { name, value } = e.target;
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+    console.log(inputValues);
+  }
   return (
     <>
       <Navigation />
       <div className="contact">
         <h1>Send us a message</h1>
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={handleSubmitForm}>
           <label htmlFor="name">Name:</label>
-          <input type="text" id="name" />
+          <input
+            required
+            type="text"
+            name="name"
+            id="name"
+            onChange={handleInputChange}
+            value={inputValues.name}
+          />
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
+          <input
+            required
+            type="email"
+            name="email"
+            id="email"
+            onChange={handleInputChange}
+            value={inputValues.email}
+          />
           <label htmlFor="message">Your message</label>
-          <textarea id="message" cols="5" rows="5"></textarea>
+          <textarea
+            required
+            name="message"
+            id="message"
+            cols="5"
+            rows="5"
+            onChange={handleInputChange}
+            value={inputValues.message}
+          ></textarea>
 
           <button type="submit" className="submit-btn">
             Send
